76f46c2eca4cfd14def50ed6d5dd9cb31bff697d
lisette926 <84044778+lisette926@users.noreply.github.com>
2024-10-30 18:01:50 -0400

    minor edit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App() {
     return (
+    <>
        <TwitterFollowCard userName="taylorswift13" name="Taylor Swift" imageSrc="https://pbs.twimg.com/profile_images/1766836345450672128/HpuBz514_400x400.jpg" />
+       <TwitterFollowCard userName="taylorswift13" name="Taylor Swift" imageSrc="https://pbs.twimg.com/profile_images/1766836345450672128/HpuBz514_400x400.jpg" />
+       <TwitterFollowCard userName="taylorswift13" name="Taylor Swift" imageSrc="https://pbs.twimg.com/profile_images/1766836345450672128/HpuBz514_400x400.jpg" />
+    </>
     )
 }
